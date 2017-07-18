0d1274f95f2ffd6c9f7ff744aee2b25263089788
Andres <andres.garcia.dev@outlook.com>
2017-07-19 00:39:20 +0200

    prueba de cambio de variables entre entornos

--- messages/index.js
+++ messages/index.js
@@ -40,6 +40,9 @@ intents.matches('Saludar', function (session, results) {
     console.log(session.message.user.id);
     console.log(session.message.user.name);
     session.send('Hola ¿En que te puedo ayudar? ' + session.message.user.id);
+
+    
+    $("#ValueReportParent").val("Dolly Duck");
 });
 
 
