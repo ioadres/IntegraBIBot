a92abc75f1595aef6e9bc286330ef1622d7f8777
Andres <Andres@iMac-de-angie.local>
2017-07-23 14:54:09 +0200

    Prueba link in new Tab

--- messages/index.js
+++ messages/index.js
@@ -8,64 +8,17 @@ const cardtemp = {
     contentType: "application/vnd.microsoft.card.adaptive",
     content: {
         type: "AdaptiveCard",
-        speak: "<s>Your  meeting about \"Adaptive Card design session\"<break strength='weak'/> is starting at 12:30pm</s><s>Do you want to snooze <break strength='weak'/> or do you want to send a late notification to the attendees?</s>",
-                     body: [{
-                "type": "TextBlock",
-                "text": "Adaptive Card design session",
-                "size": "large",
-                "weight": "bolder"
-            },
-            {
-                "type": "TextBlock",
-                "text": "Conf Room 112/3377 (10)"
-            },
-            {
-                "type": "TextBlock",
-                "text": "12:30 PM - 1:30 PM"
-            },
-            {
-                "type": "TextBlock",
-                "text": "Snooze for"
-            },
-            {
-                "type": "Input.ChoiceSet",
-                "id": "snooze",
-                "style": "compact",
-                "choices": [{
-                        "title": "5 minutes",
-                        "value": "5",
-                        "isSelected": true
-                    },
-                    {
-                        "title": "15 minutes",
-                        "value": "15"
-                    },
-                    {
-                        "title": "30 minutes",
-                        "value": "30"
-                    }
-                ]
-            }
-        ],
+        body: [{
+            "type": "TextBlock",
+            "text": "Reporte : ",
+            "size": "large",
+            "weight": "bolder"
+        }],
         "actions": [{
-                "type": "Action.Http",
-                "method": "POST",
-                "url": "http://foo.com",
-                "title": "Snooze"
-            },
-            {
-                "type": "Action.Http",
-                "method": "POST",
-                "url": "http://foo.com",
-                "title": "I'll be late"
-            },
-            {
-                "type": "Action.Http",
-                "method": "POST",
-                "url": "http://foo.com",
-                "title": "Dismiss"
-            }
-        ]
+            "Type": "openUrl",
+            "url": "http://google.com",
+            "title": "Acceder"
+        }]
     }
 };
 
@@ -107,8 +60,11 @@ const intents = new builder.IntentDialog({ recognizers: [recognizer] });
 intents.matches('Saludar', function(session, results) {
     console.log(session.message.user.id);
     console.log(session.message.user.name);
+    var currentCard = cardtemp;
+    currentCard.content.body[0].text = "Reporte Numero 2";
+
     session.send('Hola ¿En que te puedo ayudar? ' + session.message.user.id);
-    var msg = new builder.Message(session).addAttachment(cardtemp);
+    var msg = new builder.Message(session).addAttachment(currentCard);
     session.send(msg).endDialog();
 });
 
